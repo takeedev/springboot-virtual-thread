e2c662833204d7a3bf8180dcd0ca415afe09375a
taknaja <peerapolkoeram@gmail.com>
2025-02-03 20:58:52 +0700

    init project

---
+++ src/main/java/takkee/dev/SpringbootVirtualThread/SpringbootVirtualThreadApplication.java
@@ -0,0 +1,13 @@
+package takkee.dev.SpringbootVirtualThread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVirtualThreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVirtualThreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/takkee/dev/SpringbootVirtualThread/SpringbootVirtualThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package takkee.dev.SpringbootVirtualThread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVirtualThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
