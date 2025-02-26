e52e1fca3fa79ab37d29fc11d8ab9843862b354f
taknaja <peerapolkoeram@gmail.com>
2025-02-26 22:53:19 +0700

    add logback

--- src/main/java/takkee/dev/SpringbootVirtualThread/service/VirtualThreadService.java
+++ src/main/java/takkee/dev/SpringbootVirtualThread/service/VirtualThreadService.java
@@ -4,17 +4,19 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class VirtualThreadService {
 
     private final int threadCount = Runtime.getRuntime().availableProcessors();
-    private final ExecutorService executorService = Executors.newFixedThreadPool(threadCount*2);
+    private final ExecutorService executorService = Executors.newFixedThreadPool(threadCount * 2);
     private final ExecutorService executorService2 = Executors.newVirtualThreadPerTaskExecutor();
 
     public long fixedThreadPool(int task) {
         long startTime = System.currentTimeMillis();
-        System.out.println("newFixedThreadPool");
+        log.info("newFixedThreadPool");
         CompletableFuture<?>[] futures = new CompletableFuture[task];
         for (int i = 0; i < task; i++) {
             final int taskId = i;
@@ -29,7 +31,7 @@ public class VirtualThreadService {
 
     public long virtualThreadPerTaskExecutor(int task) {
         long startTime = System.currentTimeMillis();
-        System.out.println("newVirtualThreadPerTaskExecutor");
+        log.info("newVirtualThreadPerTaskExecutor");
         CompletableFuture<?>[] futures = new CompletableFuture[task];
         for (int i = 0; i < task; i++) {
             final int taskId = i;
@@ -44,7 +46,7 @@ public class VirtualThreadService {
 
     public void simulateTask(int taskId) {
         try {
-            System.out.println(Thread.currentThread() + " virsual thread : task ID => " + taskId);
+            log.info(Thread.currentThread() + " virsual thread : task ID => " + taskId);
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
